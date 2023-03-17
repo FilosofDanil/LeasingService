4b95a145c4352a7889973f72e1b6ab800a2ed697
FilosofDanil <serhio3347@gmail.com>
2023-03-17 21:15:19 +0100

    first commit

---
+++ src/main/java/com/example/wohnungsuchen/WohnungsuchenApplication.java
@@ -0,0 +1,13 @@
+package com.example.wohnungsuchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WohnungsuchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WohnungsuchenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wohnungsuchen/WohnungsuchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wohnungsuchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WohnungsuchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
